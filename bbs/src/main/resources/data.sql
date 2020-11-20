64c4a324e2b905f4f8718351933b28c0245a3738
rlackdgml <dhdlqkqh2003@naver.com>
2020-11-20 13:15:46 +0900

    bb_book

---
+++ bbs/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tbl_bbs(b_writer,b_date_time,b_content,b_count)
+values('홍길동','2020-11-18','게시판 만들기',0);
+
+INSERT INTO tbl_bbs(b_writer,b_date_time,b_content,b_count)
+values('이몽룡','2020-11-18','게시판 만들기',0);
+
+INSERT INTO tbl_bbs(b_writer,b_date_time,b_content,b_count)
+values('성춘향','2020-11-18','게시판 만들기',0);
