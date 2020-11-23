802762752b3cbaa5f7fc6cce110ec6fa4ea3b6ad
rlackdgml <dhdlqkqh2003@naver.com>
2020-11-23 14:23:40 +0900

    spring-boot

--- book/src/main/java/com/biz/book/controller/BookController.java
+++ book/src/main/java/com/biz/book/controller/BookController.java
@@ -1,49 +1,88 @@
 package com.biz.book.controller;
 
-import com.biz.book.domain.BookVO;
-import com.biz.book.service.BookService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
+import com.biz.book.domain.BookVO;
+import com.biz.book.service.BookService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping(value = "/book")
 public class BookController {
 
-    @Qualifier("bookServiceV1")
-    private final BookService bookService;
-    public BookController(BookService  bookService) {
-        this.bookService =  bookService;
-    }
+	@Qualifier("bookServiceV1")
+	private final BookService bookService;
 
-    @RequestMapping(value = "/list",method= RequestMethod.GET)
-    public String getList(Model model) {
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
 
-        List<BookVO> bookList = bookService.selectAll();
-        model.addAttribute("BOOKS",bookList);
-       return "book/list";
-        }
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String getList(@PageableDefault Pageable pageable, Model model) {
 
+		Page<BookVO> bookList = bookService.pageSelect(pageable);
+		model.addAttribute("BOOKS", bookList);
+		return "book/list";
+	}
 
-        @RequestMapping(value = "/view",method=RequestMethod.GET)
-        public String view(String id,Model model){
-        BookVO bookVO = bookService.findById(Long.valueOf(id));
-        model.addAttribute("BOOK",bookVO);
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public String view(String id, Model model) {
+		BookVO bookVO = bookService.findById(Long.valueOf(id));
+		model.addAttribute("BOOK", bookVO);
 
-        return "book/view";
+		return "book/view";
 
-        }
+	}
 
-        @RequestMapping(value = "/delete/{id}")
-        public String delete(@PathVariable("id") String id){
+	@RequestMapping(value = "/delete/{id}")
+	public String delete(@PathVariable("id") String id) {
 
-        bookService.delete(Long.valueOf(id));
-        return "redirect:/book/list";
+		bookService.delete(Long.valueOf(id));
+		return "redirect:/book/list";
 
-        }
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public String insert(@ModelAttribute BookVO bookVO, Model model) {
+		model.addAttribute("BOOK", bookVO);
+		return "book/input";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insert(@ModelAttribute BookVO bookVO) {
+
+		log.debug(bookVO.toString());
+
+		bookService.insert(bookVO);
+		return "redirect:/book/list";
+	}
+
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
+	public String update(@PathVariable("id") String id, Model model) {
+
+		BookVO bookVO = bookService.findById(Long.valueOf(id));
+		model.addAttribute("BOOK", bookVO);
+		return "book/input";
+
+	}
+
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+	public String update(@ModelAttribute BookVO bookVO, Model model) {
+
+		bookService.update(bookVO);
+		model.addAttribute("id", bookVO.getId().toString());
+		return "redirect:/book/view?id=" + bookVO.getId();
+
+	}
 }
--- book/src/main/java/com/biz/book/service/BookService.java
+++ book/src/main/java/com/biz/book/service/BookService.java
@@ -1,18 +1,20 @@
 package com.biz.book.service;
 
-import com.biz.book.domain.BookVO;
-
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.biz.book.domain.BookVO;
 
 public interface BookService {
 
     public List<BookVO> selectAll();
-
     public BookVO findById(Long id);
 
     public int insert(BookVO bookVO);
-
     public int update(BookVO bookVO);
-
     public int delete(Long id);
+
+	public Page<BookVO> pageSelect(Pageable pageable);
 }
